ad304ce5886b60c77c6274d4e9fde0bdfe2a4e19
Henning Gebhard <henning.gebhard@rub.de>
2021-12-06 11:50:34 +0100

    temporary

--- Python-Scripts/epubs/test_epubs.py
+++ Python-Scripts/epubs/test_epubs.py
@@ -6,8 +6,9 @@ have been generated from epub editions, into TEI XML.
 from unittest import TestCase
 
 from epubs import clean_up, transform
-from epubs import _wrap_paragraphs
-from epubs import _wrap_headings_and_divs
+from epubs import wrap_paragraphs
+from epubs import wrap_headings_and_divs
+from epubs import split_titlepage
 
 
 class EpubCleanupTests(TestCase):
@@ -28,12 +29,34 @@ class EpubTransformTests(TestCase):
         '''Lines should get wrapped in p tags.'''
         text = 'A line\nAnother line'
         expected = '<p>A line</p>\n<p>Another line</p>'
-        results = _wrap_paragraphs(text)
+        results = wrap_paragraphs(text)
         self.assertEqual(results, expected)
 
     def test_paragraphs_headings(self):
         '''Lines starting with "#" are no paragraphs and should be left alone.'''
         text = '# A Heading'
         expected = '# A Heading'
-        results = _wrap_paragraphs(text)
+        results = wrap_paragraphs(text)
         self.assertEqual(results, expected)
+
+    def test_split_titlepage(self):
+        '''The titlepage seems to end at the second occurence of "### ".'''
+        text = '''
+## L'Ingénu
+
+### Voltaire
+
+##### Garnier, Paris, 1877
+
+### CHAPITRE I.
+
+COMMENT LE PRIEUR DE NOTRE-DAME DE LA MONTAGNE  
+ET MADEMOISELLE SA SŒUR RENCONTRÈRENT UN HURON.
+'''
+        expected_rest = '''### CHAPITRE I.
+
+COMMENT LE PRIEUR DE NOTRE-DAME DE LA MONTAGNE  
+ET MADEMOISELLE SA SŒUR RENCONTRÈRENT UN HURON.
+'''
+        _, rest = split_titlepage(text)
+        self.assertEqual(rest, expected_rest)
